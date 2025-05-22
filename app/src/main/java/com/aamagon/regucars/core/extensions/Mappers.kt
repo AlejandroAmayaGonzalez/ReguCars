81da0209b725f3d3e012178e41b790834a880634
Alejandro <aamagon2004@gmail.com>
2025-05-22 11:46:22 +0200

    Añadir mappers entre los modelos del dominio y entidades

--- app/src/main/java/com/aamagon/regucars/core/extensions/Mappers.kt
+++ app/src/main/java/com/aamagon/regucars/core/extensions/Mappers.kt
@@ -1,10 +1,41 @@
 package com.aamagon.regucars.core.extensions
 
+import com.aamagon.regucars.data.database.entities.CarEntity
+import com.aamagon.regucars.data.database.entities.UserEntity
 import com.aamagon.regucars.data.model.CarModel
 import com.aamagon.regucars.data.model.UserModel
 import com.aamagon.regucars.domain.model.Car
 import com.aamagon.regucars.domain.model.User
 
+// Data model to Domain
 fun CarModel.toDomain() = Car(model, price, fuelType, year, colors, photo)
-
 fun UserModel.toDomain() = User(photo, nickname, email, name, birth, country)
+
+// Entities to Domain
+fun CarEntity.toDomain() = Car(model, price, fuelType, year, colors, photo)
+fun UserEntity.toDomain() = User(photo, nickname, email, name, birth, country)
+
+// Domain to Entity
+fun Car.toEntity(id: Int): CarEntity {
+    return CarEntity(
+        id = id,
+        model = model,
+        price = price,
+        fuelType = fuelType,
+        year = year,
+        colors = colors,
+        photo = photo
+    )
+}
+
+fun User.toEntity(id: Int): UserEntity {
+    return UserEntity(
+        id = id,
+        photo = photo,
+        nickname = nickname,
+        email = email,
+        name = name,
+        birth = birth,
+        country = country
+    )
+}
