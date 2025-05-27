a9d620a45f57e099cebbe23f3165e70fa4f74677
Alejandro <aamagon2004@gmail.com>
2025-05-27 14:18:07 +0200

    Permitir hacer scroll y añadir mas registros de vehículos

--- app/src/main/java/com/aamagon/regucars/ui/view/screens/MyProfileScreen.kt
+++ app/src/main/java/com/aamagon/regucars/ui/view/screens/MyProfileScreen.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -52,12 +54,15 @@ fun MyProfileScreenContent(
     userViewModel: UserViewModel,
     modifier: Modifier
 ) {
+    // Allows to scroll
+    val scrollState = rememberScrollState()
     // Access to user fields
     val user = userViewModel.user.observeAsState(User("","","","","",""))
 
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(Dimensions.default).fillMaxSize()
+        modifier = modifier.fillMaxSize().padding(Dimensions.default)
+            .verticalScroll(scrollState)
     ){
         AsyncImage(
             model = user.value.photo,
